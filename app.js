7b72be46217540afa10b4f7846f3f77c6aef6920
Adi Tiatama <adi.t@softwareseni.com>
2022-03-04 15:25:07 +0700

    remove unused code

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const express = require('express');
 const router = require('./routes/index');
 
 const app = express();
-const PORT = process.env.PORT || 3003;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
